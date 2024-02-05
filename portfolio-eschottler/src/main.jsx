8d8542c30a26a60d9d5233871c00b0638a373190
ellysaschottler <ellysastork@gmail.com>
2024-02-05 10:12:30 -0600

    created skeleton of site with pages and routing

--- portfolio-eschottler/src/main.jsx
+++ portfolio-eschottler/src/main.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+//to do? import 'bootstrap/dist/css/bootstrap.min.css'
+
+import App from './App';
+import Error from './pages/Error';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Resume from './pages/Resume';
+
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Portfolio />,
+      },
+      {
+        path: '/About',
+        element: <About />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
