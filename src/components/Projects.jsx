bec4a3bc7293031bd41b7f8fefe1e85fddacc6f8
ellysaschottler <ellysastork@gmail.com>
2024-02-07 12:52:53 -0600

    updated formatting of projects display and worked on handlepagechange function

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,15 +2,18 @@ import PropTypes from "prop-types";
 
 export default function Projects(props) {
     return (
-        <ul>
-            {props.projectsArray.map(project =>(
-                <li key={project.img}>
-                    <img src={project.img}/>
-                    <a href="{project.repo}">Repo</a>
-                    <a href="{project.deployed}">Deployed</a>
-                </li>
-
-            ))}
-        </ul>
+        
+        <div className="container">
+            <div className="row align-items-start">
+                {props.projectsArray.map(project =>(
+                    <div className="col border rounded m-2 pt-2" key={project.img}>
+                        <img src={project.img}/><br/>
+                        <a href="{project.repo}">Repo</a><br/>
+                        <a href="{project.deployed}">Deployed</a>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
+
